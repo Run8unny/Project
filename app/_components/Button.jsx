e13962f33f1824b559cda38ea427961bb8ee9f7e
Zan_ <146116869+Run8unny@users.noreply.github.com>
2024-08-04 11:02:55 +0200

    dark/light mode added

--- app/_components/Button.jsx
+++ app/_components/Button.jsx
@@ -1,7 +1,20 @@
-export default function Button({ children }) {
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = ({ onClick, children }) => {
 	return (
-		<button className='className="w-full px-3 rounded-[25px] bg-gray border-white border-[2px] border-solid box-border h-[2.5rem]'>
+		<button
+			onClick={onClick}
+			className='className="w-full px-3 rounded-[25px] bg-gray border-white border-[2px] border-solid box-border h-[2.5rem]'
+		>
 			{children}
 		</button>
 	);
-}
+};
+
+Button.propTypes = {
+	onClick: PropTypes.func,
+	children: PropTypes.node.isRequired,
+};
+
+export default Button;
--- app/_components/Cursor.jsx
+++ app/_components/Cursor.jsx
@@ -1,9 +1,9 @@
 'use client';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import gsap from 'gsap';
 
 export default function Cursor({ isHovered }) {
-	const size = isHovered ? 220 : 120;
+	const size = isHovered ? 100 : 50;
 	const circle = useRef();
 	const mouse = useRef({
 		x: 0,
@@ -32,7 +32,7 @@ export default function Cursor({ isHovered }) {
 	return (
 		<div
 			ref={circle}
-			className='fixed z-30 top-0 left-0 bg-[#ffff] rounded-full mix-blend-difference pointer-events-none'
+			className='fixed z-50 top-0 left-0 bg-[#ffff] rounded-full mix-blend-difference pointer-events-none'
 			style={{ width: size, height: size }}
 		></div>
 	);
