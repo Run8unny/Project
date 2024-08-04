798eeecec9e4bf21cf35168ff2e457a29b9eb3a9
Zan_ <146116869+Run8unny@users.noreply.github.com>
2024-08-04 18:33:02 +0200

    more responsive

--- app/_components/HomeContact.jsx
+++ app/_components/HomeContact.jsx
@@ -6,15 +6,15 @@ import Homefooter from './Homefooter';
 
 export default function HomeContact() {
 	return (
-		<div className='w-full h-screen grid grid-rows-3 md:grid-rows-3 grid-cols-5 md:grid-cols-3 relative overflow-hidden box-border leading-normal tracking-normal text-left text-[1rem] font-be-vietnam-pro p-[30px]'>
-			<div className='row-start-1 row-end-2 col-start-1 col-end-6 flex items-start justify-center text-[8rem]'>
+		<div className='w-full h-screen grid grid-rows-3 md:grid-rows-3 grid-cols-7 md:grid-cols-7 relative overflow-hidden box-border leading-normal tracking-normal text-left text-[1rem] font-be-vietnam-pro p-[30px]'>
+			<div className='row-start-1 row-end-2 col-start-1 col-end-8 flex items-start justify-center text-[8rem]'>
 				<h1 className='font-bold'>GET IN TOUCH</h1>
 			</div>
-			<div className='row-start-2 row-end-3 col-start-1 col-end-2 flex justify-center items-center justify-self-center'>
+			<div className='row-start-2 row-end-3 col-start-1 col-end-2 flex justify-center items-end justify-self-center'>
 				<Button>call me</Button>
 			</div>
 
-			<div className='text-[1.25rem] mt-40 row-start-2 row-end-3 col-start-2 col-end-5 flex flex-initial justify-center items-center'>
+			<div className='text-[1.25rem] mt-52 row-start-2 row-end-3 col-start-2 col-end-7 flex flex-initial justify-center items-center'>
 				<Circle>
 					<Link
 						href='/contact'
@@ -23,11 +23,8 @@ export default function HomeContact() {
 				</Circle>
 			</div>
 
-			<div className='row-start-2 row-end-3 col-start-5 col-end-6 flex justify-center items-center justify-self-center'>
+			<div className='row-start-2 row-end-3 col-start-7 col-end-8 flex justify-center items-end justify-self-center'>
 				<Button>mail me</Button>
-			</div>
-			<div className='row-start-3 row-end-4 col-start-1 col-end-6 flex items-end justify-center'>
-				{/* <Homefooter /> */}
 			</div>
 		</div>
 	);
