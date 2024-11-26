e7499d569c0567fc1ad365de46f48df1a812de27
Zan_ <146116869+Run8unny@users.noreply.github.com>
2024-11-26 19:58:51 +0100

    grid updated

--- app/components/LayoutGridPlayground.jsx
+++ app/components/LayoutGridPlayground.jsx
@@ -25,26 +25,25 @@ export default function LayoutGridPlayground() {
 				<div className='row-start-2 row-end-3 col-start-5 col-end-6 flex flex-col items-center justify-center p-6'>
 					<ImagePlayground src='/assets/playground_page/pic29.png' />
 				</div>
-				<div className='row-start-3 row-end-4 col-start-1 col-end-2 flex flex-col items-start justify-start p-6'>
+				<div className='row-start-3 row-end-4 col-start-1 col-end-2 flex flex-col items-center justify-center p-6'>
 					<ImagePlayground src='/assets/playground_page/pic6.png' />
 				</div>
-				<div className='row-start-3 row-end-4 col-start-5 col-end-6 flex flex-col items-start justify-start p-6'>
+				<div className='row-start-3 row-end-4 col-start-5 col-end-6 flex flex-col items-center justify-center p-6'>
 					<ImagePlayground src='/assets/playground_page/pic28.png' />
 				</div>
-				<div className='row-start-4 row-end-5 col-start-1 col-end-2 flex flex-col items-start justify-start p-6'>
+				<div className='row-start-4 row-end-5 col-start-1 col-end-2 flex flex-col items-center justify-center p-6'>
 					<ImagePlayground src='/assets/playground_page/pic25.png' />
 				</div>
-				<div className='row-start-4 row-end-5 col-start-2 col-end-3 flex flex-col items-start justify-start p-6'>
+				<div className='row-start-4 row-end-5 col-start-2 col-end-3 flex flex-col items-center justify-center p-6'>
 					<ImagePlayground src='/assets/playground_page/pic23.png' />
 				</div>
-				<div className='row-start-4 row-end-5 col-start-3 col-end-4 flex flex-col items-start justify-start p-6'>
+				<div className='row-start-4 row-end-5 col-start-3 col-end-4 flex flex-col items-center justify-center p-6'>
 					<ImagePlayground src='/assets/playground_page/pic18.png' />
 				</div>
-				<div className='row-start-4 row-end-5 col-start-5 col-end-6 flex flex-col items-start justify-start p-6'>
+				<div className='row-start-4 row-end-5 col-start-5 col-end-6 flex flex-col items-center justify-center p-6'>
 					<ImagePlayground src='/assets/playground_page/pic30.png' />
 				</div>
 				<div className='row-start-2 row-end-4 col-start-2 col-end-4 flex flex-col items-center justify-center p-20'>
-					<p>[ CREATIVE CODING 1 ]</p>
 					<video className='w-full max-w-4xl' controls>
 						<source src='/assets/playground_page/video1.mp4' type='video/mp4' />
 						Your browser does not support the video tag.
@@ -72,8 +71,7 @@ export default function LayoutGridPlayground() {
 				<div className='row-start-1 row-end-3 col-start-5 col-end-6 flex flex-col items-center justify-center p-6'>
 					<ImagePlayground src='/assets/playground_page/pic21.png' />
 				</div>
-				<div className='row-start-2 row-end-3 col-start-2 col-end-4 flex flex-col items-start justify-start p-20'>
-					<p>[ CREATIVE CODING 2 ]</p>
+				<div className='row-start-2 row-end-4 col-start-2 col-end-4 flex flex-col items-center justify-center p-20'>
 					<video className='w-full max-w-4xl' controls>
 						<source src='/assets/playground_page/video2.mp4' type='video/mp4' />
 						Your browser does not support the video tag.
@@ -87,26 +85,29 @@ export default function LayoutGridPlayground() {
 				<div className='row-start-2 row-end-3 col-start-1 col-end-2 flex flex-col items-center justify-center p-6'>
 					<ImagePlayground src='/assets/playground_page/pic15.png' />
 				</div>
-				<div className='row-start-3 row-end-4 col-start-1 col-end-2 flex flex-col items-start justify-start p-6'>
+				<div className='row-start-3 row-end-4 col-start-1 col-end-2 flex flex-col items-center justify-center p-6'>
 					<ImagePlayground src='/assets/playground_page/pic16.png' />
 				</div>
-				<div className='row-start-3 row-end-4 col-start-5 col-end-6 flex flex-col items-start justify-start p-6'>
+				<div className='row-start-3 row-end-4 col-start-5 col-end-6 flex flex-col items-center justify-center p-6'>
 					<ImagePlayground src='/assets/playground_page/pic14.png' />
 				</div>
-				<div className='row-start-4 row-end-5 col-start-1 col-end-2 flex flex-col items-start justify-start p-6'>
-					<ImagePlayground src='/assets/playground_page/pic14.png' />
+				<div className='row-start-4 row-end-5 col-start-1 col-end-2 flex flex-col items-center justify-center p-6'>
+					<ImagePlayground src='/assets/playground_page/pic4.png' />
 				</div>
-				<div className='row-start-4 row-end-5 col-start-2 col-end-3 flex flex-col items-start justify-start p-6'>
-					<ImagePlayground src='/assets/playground_page/pic14.png' />
+				<div className='row-start-4 row-end-5 col-start-2 col-end-3 flex flex-col items-center justify-center p-6'>
+					<ImagePlayground src='/assets/playground_page/pic9.png' />
 				</div>
-				<div className='row-start-4 row-end-5 col-start-3 col-end-4 flex flex-col items-start justify-start p-6'>
-					<ImagePlayground src='/assets/playground_page/pic14.png' />
+				<div className='row-start-4 row-end-5 col-start-3 col-end-4 flex flex-col items-center justify-center p-6'>
+					<ImagePlayground src='/assets/playground_page/pic15.png' />
 				</div>
-				<div className='row-start-4 row-end-5 col-start-5 col-end-6 flex flex-col items-start justify-start p-6'>
-					<ImagePlayground src='/assets/playground_page/pic14.png' />
+				<div className='row-start-4 row-end-5 col-start-4 col-end-5 flex flex-col items-center justify-center p-6'>
+					<ImagePlayground src='/assets/playground_page/pic17.png' />
+				</div>
+				<div className='row-start-4 row-end-5 col-start-5 col-end-6 flex flex-col items-center justify-center p-6'>
+					<ImagePlayground src='/assets/playground_page/pic12.png' />
 				</div>
 			</div>
-		</5
+		</>
 	);
 }
 
