2214144120c21f5339590e73a47902decea37cb0
Zan_ <146116869+Run8unny@users.noreply.github.com>
2024-11-26 16:44:14 +0100

    work page added

--- app/components/Heading.jsx
+++ app/components/Heading.jsx
@@ -1,6 +1,6 @@
 const Heading = ({ children }) => {
 	return (
-		<h1 className='w-full relative custom-font text-[7.5rem] mq1225:text-[6.5rem] mq1050:text-[6rem] mq850:text-[5.5rem] mq450:text-[4.8rem] uppercase inline-block text-orange-500 text-center h-[11rem] mt-1 outline'>
+		<h1 className='w-full relative custom-font text-[8.7rem] mq1225:text-[6.5rem] mq1050:text-[6rem] mq850:text-[5.5rem] mq450:text-[4.8rem] uppercase inline-block text-orange-500 text-center h-[11rem] outline'>
 			{children}
 		</h1>
 	);
--- app/components/Logo.jsx
+++ app/components/Logo.jsx
@@ -7,7 +7,12 @@ export default function Logo() {
 		<div className={`pl-4`}>
 			<Link href='/' className='flex flex-row'>
 				<div className='text-[40px] hover:text-orange-500 self-center'>
-					<Image width={80} height={60} src={'/assets/home/logo.png'} />
+					<Image
+						width={90}
+						height={70}
+						src={'/assets/home/logo.png'}
+						alt='logo sign'
+					/>
 				</div>
 				<div>
 					<LogoIcon />
