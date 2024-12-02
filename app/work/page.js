2c491f3b5da37c112c48aff665a0520fd4e511f6
Zan_ <146116869+Run8unny@users.noreply.github.com>
2024-12-02 19:36:19 +0100

    colors updated

--- app/work/page.js
+++ app/work/page.js
@@ -15,7 +15,9 @@ export default function Page() {
 				<MenuItem href='./playground'>playground</MenuItem>
 			</SmallNav>
 			<div className='flex items-start justify-center text-[10rem] px-10 outline xl:text-[9.8rem] lg:text-[8.5rem] mq1225:text-[7.5rem] mq1050:text-[6rem] mq850:text-[4.5rem] mq650:text-[3.8rem]'>
-				<h1 className='custom-font font-bold uppercase outline px-6'>WORK</h1>
+				<h1 className='custom-font font-bold uppercase outline px-6 text-pink-900'>
+					WORK
+				</h1>
 			</div>
 
 			<div className='w-full'>
