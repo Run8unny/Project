eb89d43d9aafd26f57eacbb0726a2811e7527b3c
Zan_ <146116869+Run8unny@users.noreply.github.com>
2024-12-05 23:58:37 +0100

    animation added

--- app/components/ContactHeading.jsx
+++ app/components/ContactHeading.jsx
@@ -1,14 +1,30 @@
 import PropTypes from 'prop-types';
+import { motion } from 'framer-motion';
 
 const ContactHeading = () => {
 	return (
-		<div
-			className={`overflow-hidden flex flex-row items-center justify-center box-border leading-[normal] tracking-[normal] text-center text-[6rem] mq675:pl-[50px] pt-14 outline`}
+		<motion.div
+			initial={{ x: '-100%' }} // Start off-screen on the left
+			animate={{ x: '100%' }} // Move to off-screen on the right
+			transition={{
+				repeat: Infinity, // Infinite looping
+				repeatType: 'loop', // Loop back to the start
+				duration: 5, // Time to complete the animation
+				ease: 'linear', // Smooth constant motion
+			}}
+			style={{
+				position: 'absolute',
+				whiteSpace: 'nowrap',
+			}}
 		>
-			<h1 className='w-auto uppercase flex justify-center items-center max-w-full text-[2.5rem] custom-font text-orange-500'>
-				Let’s CHAT * MAIL ME * leave a msg * CALL ME * LET’S TALK * Let’s CHAT
-			</h1>
-		</div>
+			<div
+				className={`overflow-hidden flex flex-row items-center justify-center box-border leading-[normal] tracking-[normal] text-center text-[6rem] mq675:pl-[50px] pt-14 outline`}
+			>
+				<h1 className='w-auto uppercase flex justify-center items-center max-w-full text-[2.5rem] custom-font text-orange-500'>
+					Let’s CHAT * MAIL ME * leave a msg * CALL ME * LET’S TALK * Let’s CHAT
+				</h1>
+			</div>
+		</motion.div>
 	);
 };
 
