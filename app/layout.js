656cb36b2628dee14ff8b2911bd522a71958286a
Zan_ <146116869+Run8unny@users.noreply.github.com>
2024-12-17 14:19:04 +0100

    bug fixed

--- app/layout.js
+++ app/layout.js
@@ -13,10 +13,7 @@ const outward = localFont({
 
 export default function RootLayout({ children }) {
 	return (
-		<html
-			lang='en'
-			className={`${cotham.variable} ${cooper.variable} ${outward.variable}`}
-		>
+		<html lang='en' className={`${cotham.variable} ${outward.variable}`}>
 			<body
 				className={`relative inset-0 w-full font-cotham bg-zinc-950 light:bg-white bg-grid-white/[0.2] light:bg-grid-black/[0.17] flex items-center justify-center xs400:p-0 xs400:m-0 lg:w-full xs400:w-fit `}
 			>
