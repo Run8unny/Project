a1b115998988e21b0798f27cebe97dd33f79d9fa
Zan_ <146116869+Run8unny@users.noreply.github.com>
2024-12-18 16:24:15 +0100

    about page updated

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Circle from './components/Circle';
 
 export default function Page() {
 	return (
-		<div className='w-full h-screen grid grid-rows-3 md:grid-rows-3 grid-cols-7 md:grid-cols-7 relative overflow-hidden box-border leading-normal tracking-normal text-left text-[1rem] p-[30px] md:w-full md:p-0 md:m-0 md:text-[0.7rem] '>
+		<div className='w-full h-screen grid grid-rows-3 md:grid-rows-3 grid-cols-7 md:grid-cols-7 relative overflow-hidden box-border leading-normal tracking-normal text-left text-[1rem] p-[30px] md:w-full md:p-0 md:m-0 md:text-[0.6rem] '>
 			<div className='w-full row-start-1 row-end-2 col-start-2 col-end-7 flex items-start justify-center'>
 				<span>[ WELCOME ]</span>
 			</div>
