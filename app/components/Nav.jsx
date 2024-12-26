bcf5c6c67d9358d888da41427cad77e87c57f459
Zan_ <146116869+Run8unny@users.noreply.github.com>
2024-12-26 19:53:54 +0100

    button changed

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,7 +4,7 @@ import Logo from './Logo';
 export default function Nav({ children }) {
 	return (
 		<div
-			className='fixed top-0 left-0 z-50 flex-grow flex flex-row justify-between items-center gap-4 md:gap-2 px-4 mb-1 bg-transparent mq750:p-3 xs400:p-0 xs400:m-0 xs400:w-full  '
+			className='fixed top-0 left-0 z-50 w-screen flex-grow flex flex-row justify-between items-center gap-4 md:gap-2 px-4 mb-1 bg-transparent mq750:p-3 xs400:p-0 xs400:m-0 xs400:w-full py-1'
 			data-scroll-to='top-of-page'
 		>
 			<div className='md:mb-0 hover:scale-150 xs400:hidden'>
--- app/components/NavMenu.jsx
+++ app/components/NavMenu.jsx
@@ -2,7 +2,7 @@ import MenuItem from './MenuItem';
 
 export default function NavMenu({ children }) {
 	return (
-		<ul className='flex flex-grow md:flex-shrink justify-center items-center text-lg max-w-2xl mx-auto'>
+		<ul className='min-w-40 flex flex-grow md:flex-shrink justify-center items-center text-lg max-w-2xl mx-auto'>
 			{children}
 		</ul>
 	);
