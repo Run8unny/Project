5381493e1c349cb0f68120cef614ece5a07c682f
Zan_ <146116869+Run8unny@users.noreply.github.com>
2024-12-26 15:02:03 +0100

    bug fixed

--- app/work/page.js
+++ app/work/page.js
@@ -17,7 +17,7 @@ export default function Page() {
 					<MenuItem href='./playground'>playground</MenuItem>
 				</NavMenu>
 			</Nav>
-			<div className='h-50 top-20 relative flex font-bold items-center justify-center  my-20 leading-none text-2xl mq850:text-xl xs400:text-[7.5rem] xs400:p-0 xs400:m-3 hover:animate-bounce '>
+			<div className='h-50 top-20 relative flex font-bold items-center justify-center  my-20 leading-none text-2xl mq850:text-xl xs400:text-[7.5rem] xs400:p-0 xs400:m-3 '>
 				<h1 className='uppercase font-bold z-50'>
 					<span className='xl:hidden'>↘︎</span>WORK
 				</h1>
